0bc037ee3fa7007bb8c128e17e483272f7b61556
Nicholas Arner <narner@unix3.csc.calpoly.edu>
2023-03-14 18:43:10 -0700

    cleaning up

--- diskfuncs.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DISKFUNCS_H
-#define _DISKFUNCS_H
-
-int check_for_partition(int image_fd);
-int check_for_superblock(int image_fd);
-int check_files(int image_fd);
-
-#endif /* _DISKFUNCS_H */
--- minls.c
+++ minls.c
@@ -1,12 +1,15 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "minhelper.h"
 
 int main(int argc, char *argv[]){
   int verbose = 0;
-  int sub_part = -1;
-  int pri_part = -1;
-  int opt;
+  int sub_part = NO_SUBPART;
+  int pri_part = NO_PRIPART;
+  int opt, offset, image_fd;
   char * image_name;
   char * path = NULL;
 
@@ -19,10 +22,12 @@ int main(int argc, char *argv[]){
       }
       case 'p':{
         printf("partition: %s\n", optarg); 
+        pri_part = atoi(optarg);
         break;
       }
       case 's':{
         printf("subpartition: %s\n", optarg);
+        sub_part = atoi(optarg);
         break;
       }
       case '?':{
@@ -31,7 +36,7 @@ int main(int argc, char *argv[]){
     }
   }
   
-  if (sub_part != -1 && pri_part == -1){
+  if (sub_part != NO_SUBPART && pri_part == NO_PRIPART){
     printf("usage: minls [-v] [ -p part [-s subpart] ] imagefile [path]\n");
     exit(EXIT_FAILURE);
   } 
@@ -45,7 +50,19 @@ int main(int argc, char *argv[]){
 
   printf("image: %s\n", image_name);
   printf("path: %s\n", path);
+  
+  /* open the image */
 
+  if ((image_fd = open(image_name, O_RDONLY)) < 0){
+    perror("open");
+    exit(EXIT_FAILURE);
+  }    
+
+  offset = get_partition(image_fd, pri_part, sub_part, verbose); 
+  
+  printf("offset: %x\n", offset);
+  
   exit(EXIT_SUCCESS); 
 }
+
 
