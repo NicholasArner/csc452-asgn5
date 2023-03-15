21ae8c8a95aa8ed26f59172024639ab24d92f318
carlysch <90992086+carlysch@users.noreply.github.com>
2023-03-14 22:25:04 -0700

    Create minls.h

---
+++ minls.h
@@ -0,0 +1,6 @@
+#ifndef MINLS_H
+#define MINLS_H
+#include "minhelper.h"
+void printCurFile(directory dir, inode i_info);
+void printDirContents(uint16_t zoneSize, superblock sb, FILE *img);
+#endif
