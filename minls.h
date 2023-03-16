f4d82e0d59873337a6c8299f6f79cd20fd8246dd
carlysch <90992086+carlysch@users.noreply.github.com>
2023-03-15 19:28:48 -0700

    Delete minls.h

--- minls.h
+++
@@ -1,7 +0,0 @@
-#ifndef MINLS_H
-#define MINLS_H
-#include "minhelper.h"
-void printCurFile(directory dir, inode i_info);
-void printDirContents(uint16_t zoneSize, superblock sb, FILE *img);
-void printPermissions(uint16_t mode);
-#endif
